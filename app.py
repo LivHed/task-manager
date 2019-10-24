34fd3f7145eec5f61fc57ac1571ac49112659077
Ubuntu <ubuntu@ip-172-31-89-81.ec2.internal>
2019-10-24 15:09:08 +0000

    Add an update function to be able to update the categories

--- app.py
+++ app.py
@@ -11,20 +11,24 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser87@myfirstcluster-kkere.mo
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template('tasks.html', tasks=mongo.db.tasks.find())
     
+    
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html', categories=mongo.db.categories.find())
+ 
     
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
+  
     
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
@@ -32,6 +36,7 @@ def edit_task(task_id):
     all_categories =  mongo.db.categories.find()
     return render_template('edittask.html', task=the_task,
                            categories=all_categories)
+ 
                            
 @app.route('/update_task/<task_id>', methods=["POST"])
 def update_task(task_id):
@@ -45,18 +50,35 @@ def update_task(task_id):
         'is_urgent':request.form.get('is_urgent')
     })
     return redirect(url_for('get_tasks'))
+
     
 @app.route('/delete_task/<task_id>')
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
+
     
 @app.route('/get_categories')
 def get_categories():
     return render_template('categories.html',
                            categories=mongo.db.categories.find())
 
-      
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.categories.find_one(
+                           {'_id': ObjectId(category_id)}))
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
